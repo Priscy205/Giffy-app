d7467cee819ec633b264d23438109cb046898b28
Priscy205 <prisslopez5@gmail.com>
2022-08-26 15:31:38 -0500

    Usando key para lista de elementos y creando componentes ListOfGits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, {useEffect, useState} from 'react';
-//import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import getGifs from './services/getGifs';
-import Gif from './components/Gif'
+import ListOfGifs from './components/ListOfGifs'
 
 export default function App() {
-  const [gifs, setGifs] = useState([])
-
-  useEffect(function (){
-    getGifs({keyword: 'programming'}).then(gifs => setGifs(gifs))
-  }, [])
-  
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(singleGif => 
-          <Gif title={singleGif.title} url={singleGif.url} id={singleGif.id} />
-          )
-        }
+        <ListOfGifs keyword='mexico'/>
       </section>
     </div>
   );
