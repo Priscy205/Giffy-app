b5e94a139559a7be1efadb065212665cac4c69d4
Priscy205 <prisslopez5@gmail.com>
2022-08-26 15:47:51 -0500

    Las dependencias del useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import getGifs from './services/getGifs';
 import ListOfGifs from './components/ListOfGifs'
 
 export default function App() {
+  const [keyword, setKeyword] = useState ('cats')
+
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword='mexico'/>
+        <button onClick={() => setKeyword ('mapache')}>Cambiar keyword</button>
+        <ListOfGifs keyword= {keyword}/>
       </section>
     </div>
   );
