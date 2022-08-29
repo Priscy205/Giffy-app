86e8bef1e6ef265299a61c83fabf5bec27ebae41
Priscy205 <prisslopez5@gmail.com>
2022-08-29 11:14:37 -0500

    Usando state de loading

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -4,12 +4,20 @@ import getGifs from '../services/getGifs';
 
 export default function ListOfGifs({ params }){
   const {keyword} = params
+  const [loading, setLoading] = useState(false)
+
   const [gifs, setGifs] = useState([])
 
   useEffect(function (){
+      setLoading(true)
       getGifs({keyword})
-        .then(gifs => setGifs(gifs))
+        .then(gifs => {
+          setGifs(gifs)
+          setLoading(false)
+        })
     }, [keyword])
+
+    if (loading) return <i>Cargando 🎶</i>
 
     return <div>{
     gifs.map(({id, title, url}) => 
