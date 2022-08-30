c3541c1917476f79bbcc3d848f3a4d9491e86f98
Priscy205 <prisslopez5@gmail.com>
2022-08-30 15:21:12 -0500

    Estilos

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Gif from './Gif'
+import './styles.css'
 
-export default function ListOfGifs({ gifs }){
+export default function ListOfGifs({gifs}){
     return <div className='ListOfGifs'>
       {
-        gifs.results.map(({id, title, url}) => 
+        gifs.map(({id, title, url}) => 
         <Gif 
+          id={id}
           key={id}
           title={title} 
           url={url} 
-          id={id} />
-    )
+           />
+      )
     }
-    </div> 
+  </div> 
 }
     
