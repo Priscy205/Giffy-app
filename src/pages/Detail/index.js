097a144a8fb465812fa6ec0e168378fac95e97fb
Priscy205 <prisslopez5@gmail.com>
2022-08-31 13:34:16 -0500

    Creación de useGlobalGifs

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,10 +1,9 @@
 import React, {useContext} from 'react'
-import GifsContext from '../../context/GifsContext'
 import Gif from '../../components/Gif'
+import useGlobalGifs from '../../hooks/useGlobalGifs'
 
 export default function Detail({params}){
-    const {gifs} = useContext(GifsContext)
-    console.log(gifs)
+    const gifs = useGlobalGifs()
 
     const gif = gifs.find(singleGif => singleGif.id = params.id)
     
