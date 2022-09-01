b1313efa9839fedd6e2d44d7fe46f4b13387a09e
Priscy205 <prisslopez5@gmail.com>
2022-09-01 16:45:46 -0500

    Paginación con React

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,14 +2,19 @@ import {useContext, useEffect, useState} from 'react'
 import getGifs from '../services/getGifs'
 import GifsContext from '../context/GifsContext'
 
+const INITIAL_PAGE = 0
+
 export function useGifs ({keyword} = {keyword: null}) {
     const [loading, setLoading]  = useState(false)
+    const [loadingNextPage, setLoadingNextPage] = useState(false)
+    const [page, setPage] = useState (0)
     const {gifs, setGifs} = useContext(GifsContext)
+
+    //recuperamos la keyword del localStorage
+    const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random'
 
     useEffect(function(){   //useEffect es una funcion que se ejecuta cada vez que se renderiza el componente
         setLoading(true)
-        //recuperamos la keyword del localStorage
-        const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'gatitos'
         
         getGifs({keyword: keywordToUse})
         .then(gifs =>{
@@ -18,7 +23,18 @@ export function useGifs ({keyword} = {keyword: null}) {
             //guardamos la keyword en el localStorage
             localStorage.setItem('lastKeyword', keyword)
         })
-    }, [keyword, setGifs])
+    }, [keyword, keywordToUse, setGifs])
 
-    return {loading, gifs}
+    useEffect(function (){
+        if (page == INITIAL_PAGE) return
+
+        setLoadingNextPage(true)
+        getGifs({keyword: keywordToUse, page})
+            .then(nextGifs =>{
+                setGifs(prevGifs => prevGifs.concat(nextGifs))
+                setLoadingNextPage(false)
+            })
+    }, [keywordToUse, page, setGifs])
+
+    return {loading, loadingNextPage, gifs, setPage}
 }
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -17,8 +17,8 @@ const fromApiResponseToGifs = apiResponse =>{
 }
 
 
-export default function getGifs({keyword = 'elephants'} = {}){
-    const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+export default function getGifs({limit =25, keyword = 'elephants', page = 0} = {}){
+    const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=g&lang=en`
 
     return fetch(apiURL)
     .then(res => res.json())
