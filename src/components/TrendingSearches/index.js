da84215bbeb55a7700754d2d7b13c73299e5da45
Priscy205 <prisslopez5@gmail.com>
2022-09-01 12:37:58 -0500

    Cargar lazy la sección de Trending

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import getTrendingTerms from '../../services/getTrendingTermsService'
 import Category from '../Category'
 
-export default function TrendingSearches (){
+function TrendingSearches (){
     const [trends, setTrends] = useState ([])
 
     useEffect (function (){
@@ -10,4 +10,25 @@ export default function TrendingSearches (){
     }, [])
 
     return <Category name='Tendencias' options={trends}/>
+}
+
+export default function LazyTrending (){
+    const[show, setShow] = useState(false)
+
+    useEffect (function (){
+        const onChange = (entries) =>{
+            const el =entries[0]
+            if(el.isIntersecting){
+                setShow(true)
+            }
+        }
+        const observer = new IntersectionObserver(onChange,{
+            rootMargin: '100px'
+        })
+        observer.observe(document.getElementById('LazyTrending'))
+    })
+
+    return <div id='LazyTrending'>
+        {show ? <TrendingSearches/> : null}
+    </div>
 }
