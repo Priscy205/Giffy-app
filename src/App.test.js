26861254f4f526f489d74b48045e6908c811b9c0
Priscy205 <prisslopez5@gmail.com>
2022-09-02 15:57:34 -0500

    Testeando eventos y formulario

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders without crashing', () => {
-  const {getByText} = render(<App />);
-  const title = screen.getByText(/learn react/i);
+  const {findByText} = render(<App />);
+  const title = await findByText (/Última búsqueda/i);
   expect(title).toBeInTheDocument();
 });
+
+//test('renders without crashing', () => {
+  //  const {getByText} = render(<App />);
+  //  const title = screen.getByText(/learn react/i);
+  //  expect(title).toBeInTheDocument();
+//  });
--- src/_test_/index.js
+++ src/_test_/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react';
+import { render, waitForElement, fireEvent, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders without crashing', () => {
-  const {getByText} = render(<App />);
-  const title = screen.getByText(/learn react/i);
-  expect(title).toBeInTheDocument();
-});
+test('home work as expected', () => {
+  const {container} = render (<App/>)
+  const gifLink = await waitForElement(
+    () => container.querySelector('.Gif-link')
+  )
+  expect(gifLink).toBeVisible()
+})
+
+test('search form could be used', async() => {
+    render (<App/>)
+    const input = await screen.findByRole('textbox')
+    const button = await screen.findByRole('button')
+
+    fireEvent.change(input, {target: { value: 'gatitos'}})
+    fireEvent.click(button)
+
+    const title = await screen.findByText('gatitos')
+    expect (title).toBeVisible
+})
+
+//test('renders without crashing', () => {
+  //  const {getByText} = render(<App />);
+  //  const title = screen.getByText(/learn react/i);
+  //  expect(title).toBeInTheDocument();
+//  });
+  
