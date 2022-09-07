40dcc0bc0a4306907b3aac53b7725aa254640fff
Priscy205 <prisslopez5@gmail.com>
2022-09-07 16:09:04 -0500

    Añadir el filtro de rating al buscador

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react'
 import { useLocation} from "wouter"
 
+const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
-function SearchForm({onSubmit}){
+
+function SearchForm(){
     const [keyword, setKeyword] = useState('')
+    const [rating, setRating] = useState(RATINGS[0])
     const [path, pushLocation] = useLocation()
 
     const handleSubmit = evt => {
@@ -16,12 +19,28 @@ function SearchForm({onSubmit}){
         setKeyword(evt.target.value)
     }
 
+    const handleChangeRating = (evt) =>{
+        setRating(evt.target.value)
+    }
+
 
     return(
-        <form onSubmit ={handleSubmit} >
-                <input id="buscar" placeholder= "Search a gif here..." onChange={handleChange} type='text' value={keyword} />
+        <form onSubmit ={handleSubmit}>
+                <input 
+                    id="buscar"    
+                    placeholder= "Search a gif here..." 
+                    onChange={handleChange} 
+                    type='text' 
+                    value={keyword} />
                 <button id="boton">Buscar</button>
-            </form>
+            <select onChange={handleChangeRating} value={rating}>
+                <option disabled> Rating type</option>
+                {RATINGS.map((rating) => (
+                <option key={rating}>{rating}</option> ))}
+            </select>
+        </form>
+        
+        
     )
 }
 
