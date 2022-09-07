06176478be36a7983ce94b9bd6f35ce833680bc7
Priscy205 <prisslopez5@gmail.com>
2022-09-07 17:40:40 -0500

    Implementando useReducer

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,15 +1,34 @@
-import React, {useState} from 'react'
+import React, {useReducer, useState} from 'react'
 import { useLocation} from "wouter"
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
+const reducer = (state, param) =>{
+    return {
+        ...state,
+        keyword: param,
+        times: state.times + 1
+    }
+}
+
 
 export default function SearchForm({initialKeyword= '', initialRating='g'}){
-    const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword))
+    //const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword))
     const [rating, setRating] = useState(initialRating)
-    const [times, setTimes] = useState (0)
+    //const [times, setTimes] = useState (0)
+
+    const [state, dispatch]=useReducer(reducer, {
+        keyword: decodeURIComponent(initialKeyword),
+        times: 0
+    })    //dispatch permite lanzar acciones para actualizar ese estado 
+
+    const {keyword, times} =state
 
     const [path, pushLocation] = useLocation()
+
+    const updateKeyword = (keyword) => {
+        dispatch(keyword)
+    }
 
     const handleSubmit = evt => {
         evt.preventDefault()
@@ -18,8 +37,7 @@ export default function SearchForm({initialKeyword= '', initialRating='g'}){
     }
 
     const handleChange = evt =>{
-        setKeyword(evt.target.value)
-        setTimes(times + 1)
+        updateKeyword(evt.target.value)
     }
 
     const handleChangeRating = (evt) =>{
