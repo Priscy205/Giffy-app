d27fe5433c076943c536a4a6d20a566c9adf6bf3
Priscy205 <prisslopez5@gmail.com>
2022-09-08 16:47:38 -0500

    Tests

--- src/components/SearchForm/hook.test.js
+++ src/components/SearchForm/hook.test.js
@@ -1,9 +1,32 @@
-import {renderHook} from '@testing-libraryreact@^13.1'
+import {act, renderHook} from '@testing-libraryreact@^13.1'
 import useForm from './hook'
 
 test ('should change keyword',() => {
     const {result} = renderHook(() => useForm())
-    result.current.updateKeyword('batman')
+    
+    act(()=>{ //simula como react se comporta en un navegador
+        result.current.updateKeyword('batman')
+    })
     
     expect(result.current.keyword).toBe('batman')
+})
+
+test ('should use initial values', () =>{
+    const {result} = renderHook(()=> useForm({
+        initialKeyword: 'matrix'
+    }))
+    expect (result.current.keyword).toBe('matrix')
+})
+
+test ('should update correctly times when used twice', () =>{
+    const {result} = renderHook(()=> useForm({
+        initialKeyword: 'matrix'
+    }))
+    act(()=>{ //simula como react se comporta en un navegador
+        result.current.updateKeyword('b')
+        result.current.updateKeyword('ba')
+
+    })
+    expect (result.current.keyword).toBe('ba')
+    expect(result.current.times).toBe(2)
 })
